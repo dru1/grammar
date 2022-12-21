9fde3b89a27f0e018abfab540386895847f64cf1
daniel <dru1441@gmail.com>
2022-12-21 22:03:30 +0100

    Gradle 7.6, Antlr 4.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,19 +12,19 @@ repositories {
 
 dependencies {
     // Plugin ANTLR version 4
-    antlr("org.antlr:antlr4:4.8-1")
+    antlr("org.antlr:antlr4:4.9.3")
 
     // Logging
-    implementation("org.slf4j:slf4j-api:1.7.25")
-    runtimeOnly("ch.qos.logback:logback-classic:1.2.3")
+    implementation("org.slf4j:slf4j-api:2.0.6")
+    runtimeOnly("ch.qos.logback:logback-classic:1.3.5")
 
     // Utils
     implementation("com.google.code.findbugs:jsr305:3.0.2")
-    implementation("com.google.guava:guava:28.2-jre")
-    implementation("org.apache.commons:commons-lang3:3.9")
+    implementation("com.google.guava:guava:31.1-jre")
+    implementation("org.apache.commons:commons-lang3:3.12.0")
 
     // Tests
-    testImplementation("junit:junit:4.12")
+    testImplementation("junit:junit:4.13.2")
 }
 
 tasks.generateGrammarSource {
